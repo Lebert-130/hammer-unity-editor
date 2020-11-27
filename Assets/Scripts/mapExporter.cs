3f94c5d21e33538b5c82d5269db6c03032989aba
Lebert130 <51353708+Lebert130@users.noreply.github.com>
2020-11-26 19:19:34 -0500

    Add files via upload

---
+++ Assets/Scripts/mapExporter.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class mapExporter : MonoBehaviour {
+
+    string m_Path;
+
+    public GameObject[] blocks;
+	
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            m_Path = Application.dataPath + "/test.map";
+
+            if (File.Exists(m_Path))
+            {
+                File.Delete(m_Path);
+            }
+
+            //Begin file creation
+            using (FileStream fs = File.Create(m_Path))
+            {
+                Setup(fs);
+                RegisterBlocks(fs);
+                AddText(fs, "}");
+            }
+        }
+	}
+
+    void Setup(FileStream fs)
+    {
+        AddText(fs, "{\n");
+        AddText(fs, "\"classname\" \"worldspawn\"\n");
+        AddText(fs, "\"classname\" \"worldspawn\"\n");
+        AddText(fs, "\"sounds\" \"1\"\n");
+        AddText(fs, "\"MaxRange\" \"4096\"\n");
+        AddText(fs, "\"mapversion\" \"220\"\n");
+        AddText(fs, "\"wad\" \"\\program files (x86)\\steam\\steamapps\\common\\half-life\\valve\\halflife.wad\"\n");
+    }
+
+    void RegisterBlocks(FileStream fs)
+    {
+        blocks = GameObject.FindGameObjectsWithTag("Plane");
+
+        AddText(fs, "{\n");
+
+        foreach(GameObject block in blocks)
+        {
+            var vertex1 = "(" + block.GetComponent<meshGenerator>().vertex1.x.ToString() + " " + block.GetComponent<meshGenerator>().vertex1.z.ToString() + " " + block.GetComponent<meshGenerator>().vertex1.y.ToString() + ")";
+            var vertex2 = "(" + block.GetComponent<meshGenerator>().vertex2.x.ToString() + " " + block.GetComponent<meshGenerator>().vertex2.z.ToString() + " " + block.GetComponent<meshGenerator>().vertex2.y.ToString() + ")";
+            var vertex3 = "(" + block.GetComponent<meshGenerator>().vertex3.x.ToString() + " " + block.GetComponent<meshGenerator>().vertex3.z.ToString() + " " + block.GetComponent<meshGenerator>().vertex3.y.ToString() + ")";
+
+            if (block.gameObject.name == "right" || block.gameObject.name == "left")
+            {
+                vertex3 += " FIFTIES_FLR03 [ 0 1 0 0 ] [ 0 0 -1 0 ] 0 1 1\n";
+            }
+            else if (block.gameObject.name == "back" || block.gameObject.name == "front")
+            {
+                vertex3 += " FIFTIES_FLR03 [ 1 0 0 0 ] [ 0 0 -1 0 ] 0 1 1\n";
+            }
+            else if (block.gameObject.name == "top" || block.gameObject.name == "bottom")
+            {
+                vertex3 += " FIFTIES_FLR03 [ 1 0 0 0 ] [ 0 -1 0 0 ] 0 1 1\n";
+            }
+
+            AddText(fs, vertex1);
+            AddText(fs, vertex2);
+            AddText(fs, vertex3);
+        }
+
+        AddText(fs, "}\n");
+    }
+
+    private static void AddText(FileStream fs, string value)
+    {
+        byte[] info = Encoding.ASCII.GetBytes(value);
+        fs.Write(info, 0, info.Length);
+    }
+
+}
---
+++ Assets/Scripts/meshGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class meshGenerator : MonoBehaviour {
+
+    public Mesh mesh;
+
+    public Vector3 vertex1;
+    public Vector3 vertex2;
+    public Vector3 vertex3;
+    public Vector3 vertex4;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+	void Start () {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+	}
+
+    void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            vertex1,
+            vertex2,
+            vertex3,
+            vertex4
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2,
+            1, 3, 2
+        };
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+}
